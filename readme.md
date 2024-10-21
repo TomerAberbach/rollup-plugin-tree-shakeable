41bfffbfddcc3545cbe3468cf0ef5bdce65a3aaa
Tomer Aberbach <tomer@aberba.ch>
2024-10-20 22:58:15 -0400

    docs: use mit license moving forward

--- readme.md
+++ readme.md
@@ -124,11 +124,9 @@ Stars are always welcome!
 For bugs and feature requests,
 [please create an issue](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/issues/new).
 
-For pull requests, please read the
-[contributing guidelines](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/blob/main/contributing.md).
-
 ## License
 
-[Apache License 2.0](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/blob/main/license)
-
-This is not an official Google product.
+[MIT](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/blob/main/license)
+© [Tomer Aberbach](https://github.com/TomerAberbach) \
+[Apache 2.0](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/blob/main/license-apache)
+© [Google](https://github.com/TomerAberbach/rollup-plugin-tree-shakeable/blob/main/notice-apache)
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,3 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { rollup } from 'rollup'
@@ -36,23 +20,7 @@ test(`treeShakeable works`, async () => {
   await output.close()
 
   expect(rollupOutput).toMatchInlineSnapshot(`
-"/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-const logAndReturn = x => {
+"const logAndReturn = x => {
   console.log(\`log\`);
   return x
 };
